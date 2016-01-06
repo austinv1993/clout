0d965eb8849167a169745f17dc8517254e465dbb
OdieD8 <odie_de_moura@hotmail.com>
2016-01-05 18:09:48 -0700

    Added newWorkout Service

--- www/js/services/workoutCreationSrvc.js
+++ www/js/services/workoutCreationSrvc.js
@@ -1,2 +1,16 @@
 angular.service('workoutCreationSrvc', ['$q', '$http', workoutCreationCtrl]);
 
+function workoutCreationSrvc($q, $http) {
+	
+	this.addNewWorkout = function(newWorkout) {
+		
+		return $http.post('/api/', newWorkout)
+			.then(function(response) {
+				
+				return "Workout Added";
+			}, function(error) {
+				
+				return error;
+			});
+	};
+};
