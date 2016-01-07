4d0a345c6847d2b6f4852174bc5d800b407bdbfc
Austin Vance <austinv_1993@yahoo.com>
2016-01-07 11:07:07 -0700

    commit

--- www/js/controllers/workoutSelectionCtrl.js
+++ www/js/controllers/workoutSelectionCtrl.js
@@ -6,12 +6,19 @@ angular.module('clout')
         });
     };
     $scope.getFirstPage();
+    $scope.endOfResults = false;
     
     $scope.getMoreWorkouts = function() {
         workoutsService.offset += 6;
         workoutsService.getWorkouts().then(function(workouts) {
             $scope.workouts = workouts;
             console.log(workoutsService.offset);
+            if ($scope.workouts.length > 0) {
+                $scope.endOfResults = false;
+            } else {
+                $scope.endOfResults = true;
+            }
+            console.log($scope.endOfResults);
         });
     };
     
@@ -19,6 +26,11 @@ angular.module('clout')
         workoutsService.offset -= 6;
         workoutsService.getPreviousWorkouts().then(function(workouts) {
             $scope.workouts = workouts;
+            if ($scope.workouts.length > 0) {
+                $scope.endOfResults = false;
+            } else {
+                $scope.endOfResults = true;
+            }
         });
     };
 });
