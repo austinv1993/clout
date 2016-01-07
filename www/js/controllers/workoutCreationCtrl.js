8fb68a6810c9becc03771e32775566b9aa605843
OdieD8 <odie_de_moura@hotmail.com>
2016-01-07 11:35:06 -0700

    WorkoutCreation Front-End Working

--- www/js/controllers/workoutCreationCtrl.js
+++ www/js/controllers/workoutCreationCtrl.js
@@ -6,6 +6,8 @@ app.controller('workoutCreationCtrl', ['$scope', '$state', '$stateParams', 'work
 		
 		$scope.newExercise = {};
 		
+		$scope.workout = {};
+		
 		$scope.exercises = [];
 		
 		$scope.addNewExercise = function() {
@@ -24,16 +26,18 @@ app.controller('workoutCreationCtrl', ['$scope', '$state', '$stateParams', 'work
 			array.splice(index, 1);
 		};
 		
+		$scope.newEquipment = {};
+		
 		$scope.equipments = [];
 		
 		$scope.addNewEquipment = function() {
 			
-			if($scope.newEquipment === "") {
+			if(!$scope.newEquipment.text) {
 				alert("Please enter equipment");
 			}
 			else {
-				$scope.equipments.push($scope.newEquipment);
-				$scope.newEquipment = "";
+				$scope.equipments.push($scope.newEquipment.text);
+				$scope.newEquipment = {};
 			}
 		};
 		
@@ -41,26 +45,94 @@ app.controller('workoutCreationCtrl', ['$scope', '$state', '$stateParams', 'work
 			
 			array.splice(index, 1);
 		};
-				
-		$scope.newWorkout = {
-			name: $scope.newWorkoutName,
-			workoutType: $scope.newWorkoutType,
-			exercises: $scope.exercises,
-			equipment: $scope.equipments,
-			level: $scope.level,
-			time: $scope.estWorkoutTime,
-			description: $scope.description
+		
+		$scope.reps = false;
+		$scope.interval = false;
+		
+		$scope.workoutType = function() {
+			if($scope.workout.type === "reps") {
+				$scope.reps = true;
+				$scope.interval = false;
+			}
+			if($scope.workout.type === "interval") {
+				$scope.interval = true;
+				$scope.reps = false;
+			}
 		};
 		
-		$scope.postNewWorkout = function() {
+		$scope.errorMessages = [];
 		
-			workoutCreationSrvc.addNewWorkout($scope.newWorkout).then(function (data) {
-				
-				alert("Workout Added");
-				$state.go('tab.workout-selection');
-				
-			});
-		}
+		$scope.postNewWorkout = function() {
+			
+			$scope.workoutTime = {
+				mins: $scope.workout.mins,
+				secs: $scope.workout.secs
+			};
+			
+			$scope.workoutReps = {
+				reps: $scope.workout.reps,
+				sets: $scope.workout.sets
+			};
+			
+			$scope.newWorkout = {
+				name: $scope.workout.name,
+				workoutType: $scope.workout.type,
+				exercises: $scope.exercises,
+				equipment: $scope.equipments,
+				level: $scope.workout.level,
+				time: $scope.workoutTime,
+				reps: $scope.workoutReps,
+				description: $scope.workout.description
+			};
+			
+			if(!$scope.workout.name) {
+				$scope.errorMessages.push(" name");
+			}
+			if(!$scope.workout.type) {
+				$scope.errorMessages.push(" type");
+			}
+			if($scope.exercises.length === 0) {
+				$scope.errorMessages.push(" exercises");
+			}
+			if($scope.equipments.length === 0) {
+				$scope.errorMessages.push(" equipment");
+			}
+			if(!$scope.workout.level) {
+				$scope.errorMessages.push(" level");
+			}
+			if($scope.workout.type === "interval") {
+				if(!$scope.workoutTime) {
+					console.log("got to interval");
+					$scope.errorMessages.push(" estimated time");
+				}
+			}
+			if($scope.workout.type === "reps") {
+				if(!$scope.workoutReps) {
+					console.log("got to reps");
+					$scope.errorMessages.push(" reps/sets");
+				}
+			}
+			
+			if(!$scope.workout.description) {
+				$scope.errorMessages.push(" description");
+			}
+			
+			if($scope.errorMessages.length > 0) {
+				alert("The following are missing: " + $scope.errorMessages);
+				$scope.errorMessages = [];
+			}
+			
+			else {	
+				workoutCreationSrvc.addNewWorkout($scope.newWorkout).then(function (data) {
+					
+					console.log($scope.workout.name);
+					console.log($scope.newWorkout);
+					alert("Workout Added");
+					$state.go('tab.workout-selection');
+					
+				});
+			}
+		};
 };
 
 
