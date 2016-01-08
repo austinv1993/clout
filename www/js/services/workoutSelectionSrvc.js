3ec7bdcbc3d52c2bbe290ea809f8a1ddd5b756d6
Mariano Vargas <mario.vargas29@gmail.com>
2016-01-08 10:13:51 -0700

    created new template

--- www/js/services/workoutSelectionSrvc.js
+++ www/js/services/workoutSelectionSrvc.js
@@ -2,15 +2,15 @@ angular.module('clout')
 .service('workoutSelectionSrvc', function($http) {
     this.offset = 0;
     this.pageCount = 6;
-    
+
     this.getWorkouts = function() {
-        return $http.get('http://localhost:3000/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
             return response.data;
         });
     };
-    
+
     this.getPreviousWorkouts = function() {
-        return $http.get('http://localhost:3000/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
             return response.data
         });
     };
