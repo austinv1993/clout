3bbf69154c895ac5869ba3a30aaea876ee8a9746
OdieD8 <odie_de_moura@hotmail.com>
2016-01-08 15:33:37 -0700

    merging w-timer

--- www/js/services/workoutSelectionSrvc.js
+++ www/js/services/workoutSelectionSrvc.js
@@ -4,19 +4,19 @@ angular.module('clout')
     this.pageCount = 6;
     
     this.getWorkouts = function() {
-        return $http.get('http://localhost:3000/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
             return response.data;
         });
     };
     this.getWorkoutById = function(workoutId) {
-        return $http.get('http://localhost:3000/api/workout?workoutId=' + workoutId).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workout?workoutId=' + workoutId).then(function(response) {
             console.log(response.data);
             return response.data;
         });
     };
     
     this.getPreviousWorkouts = function() {
-        return $http.get('http://localhost:3000/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
             return response.data;
         });
     };
