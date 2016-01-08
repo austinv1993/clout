84ea94c3ec3a28102c30b8d21e5cabe8704ad3a0
theDakotaCorbin <dakotamcorbin@gmail.com>
2016-01-07 17:38:24 -0700

    Updated some of the styling

--- www/js/services/workoutSelectionSrvc.js
+++ www/js/services/workoutSelectionSrvc.js
@@ -4,13 +4,13 @@ angular.module('clout')
     this.pageCount = 6;
     
     this.getWorkouts = function() {
-        return $http.get('http://localhost:3000/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
             return response.data;
         });
     };
     
     this.getPreviousWorkouts = function() {
-        return $http.get('http://localhost:3000/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
+        return $http.get('https://blooming-shore-7771.herokuapp.com/api/workouts?offset=' + this.offset + '&count=' + this.pageCount).then(function(response) {
             return response.data
         });
     };
