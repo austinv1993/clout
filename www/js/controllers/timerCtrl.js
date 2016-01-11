c6b01b9187eba68b70c03fa8c61062e46b12dede
OdieD8 <odie_de_moura@hotmail.com>
2016-01-11 16:38:38 -0700

    Timer functional

--- www/js/controllers/timerCtrl.js
+++ www/js/controllers/timerCtrl.js
@@ -4,46 +4,37 @@ app.controller('timerCtrl', ['$scope', '$state', '$stateParams', '$timeout', 'wo
 
 function timerCtrl($scope, $state, $stateParams, $timeout, workoutSelectionSrvc) {
 
-	$scope.getCounter = function() {
+    $scope.getCounter = function () {
 
-        workoutSelectionSrvc.getWorkoutById($stateParams.workoutId).then(function(data) {
-            
-            $scope.exerciseInterval = data.exercises;
-            $scope.counter = $scope.exerciseInterval[0].mins;
+        workoutSelectionSrvc.getWorkoutById($stateParams.workoutId).then(function (data) {
+
+            var counter = 0;
+
+            for (var i = 0; i < data.exercises.length; i++) {                        
+                var exerciseInterval = data.exercises;
+                var counterMins = exerciseInterval[i].mins;
+                var counterSecs = exerciseInterval[i].secs;
+                $scope.exerciseTime = parseInt(counterMins) * 60 + parseInt(counterSecs) + 1;
+                $scope.exerciseName = exerciseInterval[i].name;
+                console.log(counter);
+                counter++;
+                angular.element(".stopwatch").attr("data-timer", $scope.exerciseTime);
+    
+                $(".stopwatch").TimeCircles({ start: true });
+                $(".stopwatch").TimeCircles();
+                $(".start").click(function () { $(".stopwatch").TimeCircles().start(); });
+                $(".stop").click(function () { $(".stopwatch").TimeCircles().stop(); });
+                $(".restart").click(function () { $(".stopwatch").TimeCircles().restart(); });
+                $(".stopwatch").TimeCircles().addListener(function(unit, value, total) {
+                    if(total === 0) {
+                        alert("DONE!");
+                    }
+                });
+            }
         });
     };
-    
+
     $scope.getCounter();
 
-    var mytimeout = null; // the current timeoutID
- 
-    // actual timer method, counts down every second, stops on zero
-    $scope.onTimeout = function () {
-        
-        if ($scope.counter === 0) {
-            $scope.$broadcast('timer-stopped', 0);
-            $timeout.cancel(mytimeout);
-            return;
-        }
-        $scope.counter--;
-        mytimeout = $timeout($scope.onTimeout, 1000);
-    };
-
-    $scope.startTimer = function () {
-        mytimeout = $timeout($scope.onTimeout, 1000);
-    };
- 
-    // stops and resets the current timer
-    $scope.stopTimer = function () {
-        $scope.$broadcast('timer-stopped', $scope.counter);
-        $scope.counter = 3;
-        $timeout.cancel(mytimeout);
-    };
- 
-    // triggered, when the timer stops, you can do something here, maybe show a visual indicator or vibrate the device
-    $scope.$on('timer-stopped', function (event, remaining) {
-        if (remaining === 0) {
-            alert('Finished!');
-        }
-    });
 };
+
