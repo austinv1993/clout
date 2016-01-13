0d80a7d10f608e8bf266b0bf5183adb578941033
Austin Vance <austinv_1993@yahoo.com>
2016-01-12 17:43:02 -0700

    auth working through postman

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express = require('express')
 ,   passport = require('passport')
 ,   jwt = require('jwt-simple')
 ,   mongoose = require('mongoose')
+,   User = require('./server/models/User.js')
 ,   mongooseUri = secret.mongooseUri
 ,   WorkoutCtrl = require('./server/controllers/WorkoutCtrl.js')
 ,   UserCtrl = require('./server/controllers/UserCtrl.js')
@@ -20,15 +21,19 @@ app.use(morgan('dev'));
 app.use(passport.initialize());
 app.use(express.static(__dirname + './www'));
 
+
+var apiRoutes = express.Router();
+app.use('/api', apiRoutes);
+
 //WORKOUTS//
-app.get('/api/workouts', WorkoutCtrl.allWorkouts);
-app.post('/api/workouts', WorkoutCtrl.createWorkout);
-app.get('/api/workout', WorkoutCtrl.findById);
+apiRoutes.get('/workouts', WorkoutCtrl.allWorkouts);
+app.post('/workouts', WorkoutCtrl.createWorkout);
+app.get('/workout', WorkoutCtrl.findById);
 
 //USERS//
-app.post('/api/newuser', UserCtrl.createUser);
-app.get('/api/userbyid', UserCtrl.getById);
-app.get('/api/users', UserCtrl.getUsers);
+app.post('/newuser', UserCtrl.createUser);
+app.get('/userbyid', UserCtrl.getById);
+app.get('/users', UserCtrl.getUsers);
 
 
 
@@ -38,25 +43,41 @@ mongoose.connect(mongooseUri);
 
 require('./config/passport')(passport);
 
-var apiRoutes = express.Router();
 
-apiRoutes.post('/signup', function(req, res) {
-    if (!req.body.name || !req.body.password) {
-        res.json({success: false, msg: 'Please pass name and password.'});
-    } else {
-        var newUser = new User({
-            name: req.body.name,
-            password: req.body.password
-        });
-        newUser.save(function(err) {
-            if (err) {
-                return res.json({success: false, msg: 'Username already exists.'});
-            }
-            res.json({success: true, msg: 'Successfully created new user.'});
-        });
-    }
+
+apiRoutes.post('/signup', UserCtrl.signup);
+apiRoutes.post('/authenticate', UserCtrl.authenticate);
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, mongooseUri);
+    User.findOne({
+      name: decoded.name
+    }, function(err, user) {
+        if (err) throw err;
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+          res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
 });
-app.use('/api', apiRoutes);
+var getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
 mongoose.connection.once('open', function() {
 	console.log("Connected to MongoDB");
     
--- server/controllers/UserCtrl.js
+++ server/controllers/UserCtrl.js
@@ -27,6 +27,46 @@ module.exports = {
                 res.send(users);
             }
         })
+    },
+    signup: function(req, res) {
+        if (!req.body.name || !req.body.password) {
+            res.json({success: false, msg: 'Please pass name and password.'});
+        } else {
+            var newUser = new User({
+                name: req.body.name,
+                password: req.body.password
+            });
+            newUser.save(function(err) {
+                if (err) {
+                    return res.json({success: false, msg: 'Username already exists.'});
+                }
+                res.json({success: true, msg: 'Successfully created new user.'});
+            });
+        }
+    },
+    authenticate: function(req, res) {
+        User.findOne({
+            name: req.body.name
+        }, function(err, user) {
+            if (err) throw err;
+        
+            if (!user) {
+            res.send({success: false, msg: 'Authentication failed. User not found.'});
+            } else {
+            // check if password matches
+            user.comparePassword(req.body.password, function (err, isMatch) {
+                if (isMatch && !err) {
+                // if user is found and password is right create a token
+                var token = jwt.encode(user, mongooseUri); //not sure if this right
+                // return the information including token as JSON
+                res.json({success: true, token: 'JWT ' + token});
+                } else {
+                res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+                }
+            });
+            }
+        });
     }
+    
     
 };
