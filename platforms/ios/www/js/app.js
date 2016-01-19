d4240435314e0396e0abe8d856c13a34d5badbe3
Austin Vance <austinv_1993@yahoo.com>
2016-01-19 11:19:31 -0700

    temporary

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,58 @@
+angular.module('clout', ['ionic', 'clout.controllers', 'clout.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  
+  $stateProvider
+
+  
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })                                                                                                                                                                                            
+  .state('tab.dash', {                      
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
+  .state('tab.workout-selection', {
+    url: '/workout-selection',
+    views: {
+      'tab-workout-selection': {
+        templateUrl: 'templates/tab-workout-selection.html',
+        controller: 'workoutSelectionCtrl'
+      }
+    }
+  })
+  
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
---
+++ platforms/ios/www/js/services/services.js
@@ -0,0 +1,5 @@
+angular.module('clout.services', [])
+
+.factory('Chats', function() {
+  
+});
