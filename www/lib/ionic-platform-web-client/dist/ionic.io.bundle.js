19f8e5ad63a9ee1e12a67cf34772b445a00e7919
OdieD8 <odie_de_moura@hotmail.com>
2016-01-20 12:06:46 -0700

    changes for deployment

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3440,7 +3440,7 @@ var BaseSettings = (function () {
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
 
-  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"0b34712a","api_key":"ba541ca242a38b7535c5dfdf524227023745e1ca23b4189c","dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"175f02fc","api_key":"cda670d6c3d6e8b9bb96aab6f3a4f8879a96ca088d02dd52","dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 
 }).finish();
 
